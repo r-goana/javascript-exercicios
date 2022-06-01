1d5b6b7e4bf2f5f52fe98569aa9d9a2a145c0c73
G0ana <rafaelagoes.contato@hotmail.com>
2022-06-01 20:37:45 -0300

    first commit

---
+++ exercicio1.js
@@ -0,0 +1,6 @@
+//Exercício para criar variáveis com os tipos de dados que aprendemos em sala de aula.
+//String, int, double, float, boolean
+
+//STRING
+
+console.log("Olá mundo!");
---
+++ variaveis.js
@@ -0,0 +1,27 @@
+//Exercício para criar variáveis com os tipos de dados que aprendemos em sala de aula.
+//String, int, double, float, boolean
+
+//console.log("Olá mundo!");
+
+//STRING
+const Nome = "Rafaela";
+console.log(Nome);
+
+let idade = 24
+console.log(idade);
+
+var conta = (321*1.23)+(321*3.26);
+var arredondado = parseFloat(conta.toFixed(2));
+
+console.log(conta); 
+console.log(arredondado); 
+
+let verdadeiro = true;
+let falso = false;
+
+let maior = 5 > 12;
+let menor = 10 < 20;
+console.log(verdadeiro);
+console.log(falso);
+console.log(maior);
+console.log(menor);
