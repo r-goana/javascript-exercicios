96d3fb793da3aef98f6342da0a74985f58a6ac92
G0ana <rafaelagoes.contato@hotmail.com>
2022-06-01 20:50:20 -0300

    exercicio2

--- variaveis2.js
+++ variaveis2.js
@@ -2,4 +2,4 @@ let distanciaPercorrida = 380;
 let tempoGasto = 8;
 let velocidadeMedia = distanciaPercorrida/tempoGasto;
 
-console.log('A velocidade média que fiz o trajeto é de ${velocidadeMedia} km/h.')
+console.log(`A velocidade média que fiz o trajeto é de ${velocidadeMedia} km/h.`)
